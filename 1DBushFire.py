00268874445faa2e5420d55fa4f3083d1f76e79b
jeramirezcarrasco <32339514+jeramirezcarrasco@users.noreply.github.com>
2020-07-16 20:17:12 -0600

    Create 1DBushFire.py

---
+++ 1DBushFire.py
@@ -0,0 +1,39 @@
+# Difficulty: Easy
+# Language: Python
+# Time: 30 minutes
+# Date: 7/16/2020
+
+# Given the location of fire on the strip, you have to command the firefighter to put out all fire with the least number of water drops.
+
+# https://www.codingame.com/ide/puzzle/1d-bush-fire
+
+
+import sys
+import math
+
+n = int(input())
+forest = []
+for i in range(n):
+    forest += [input()]
+for i in forest:
+    count = 0
+    fire = 0
+    for j in range(0, len(i)):
+        if(i[j] == 'f'):
+            fire +=1
+            if(fire == 3):
+                count +=1
+                fire = 0
+        elif(fire == 2):
+            count +=1
+            fire = 0
+        elif(fire == 1):
+            if(j < len(i)-1 and i[j+1] == 'f'):
+                count +=1
+                fire = -1
+            else:
+                count +=1
+                fire = 0
+    if(fire > 0):
+        count +=1
+    print(count)
